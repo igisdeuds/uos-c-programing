511d513d794ac23ed58e99456d8830f862d1a691
igisdeuds <126849537+igisdeuds@users.noreply.github.com>
2023-04-12 12:56:32 +0900

    Create hw2header.h

---
+++ 20230407 hw.2/20230407 hw.2/hw2header.h
@@ -0,0 +1,6 @@
+﻿int minP(int a, int b);	//기존 변수에서 최소공배수 찾는 함수
+int maxP(int a, int b);	//기존 변수에서 최대공약수 찾는 함수
+int primeP(int a);		//기존 변수에서 소수 판별하는 함수
+int minS(void);			//변수 입력받아 최소공배수 찾는 함수
+int maxS(void);			//변수 입력받아 최대공약수 찾는 함수
+int primeS(void);		//변수 입력받아 소수판별 찾는 함수
